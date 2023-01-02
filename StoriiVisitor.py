968928ef840c8bf7f91fae6e61879f0ced426fd7
johnryanmal <johnryanmal@gmail.com>
2023-01-01 21:56:15 -0800

    add visitor

---
+++ StoriiVisitor.py
@@ -0,0 +1,5 @@
+from StoriiParserVisitor import StoriiParserVisitor
+
+
+class StoriiVisitor(StoriiParserVisitor):
+    pass
--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import sys
 from antlr4 import *
 from StoriiLexer import StoriiLexer
 from StoriiParser import StoriiParser
+from StoriiVisitor import StoriiVisitor
 
 
 def main(argv):
     input_stream = FileStream(argv[1])
     lexer = StoriiLexer(input_stream)
+    # print(list(token.type for token in lexer.getAllTokens()))
     stream = CommonTokenStream(lexer)
     parser = StoriiParser(stream)
     tree = parser.program()
-    print(tree)
+    visitor = StoriiVisitor()
+    ast = visitor.visitProgram(tree)
+    print(ast)
 
 
 if __name__ == '__main__':
